44897cbf674ef902e906e0e7c155d8137e81f8e1
Pavel Petronjuk <hardboatguy@gmail.com>
2024-11-17 17:13:08 +0200

    create and modify Names_map; initial commit;

---
+++ src/app/DataHandler.java
@@ -0,0 +1,33 @@
+package app;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class DataHandler {
+
+    private final Map<Integer, String> map;
+
+    public DataHandler() {
+        this.map = new DataRepository().getData();
+    }
+
+    // Метод формує виведення нумерованого переліку імен
+    public String getAll() {
+        StringBuilder sb = new StringBuilder();
+        AtomicInteger count = new AtomicInteger(0);
+        map.forEach((id, name) ->
+                sb.append(String.format("%d) %d, %s%n",
+                        count.incrementAndGet(), id, name)
+                ));
+        return "\nALL NAMES:\n" + sb;
+    }
+
+    // Метод формує виведення імені за певним id
+    public String getById(int id) {
+        if (map.containsKey(id)) {
+            return "\nNAME: id " + id + ", " + map.get(id);
+        } else {
+            return "No data!";
+        }
+    }
+}
